55a6b72e3ae969a3b40b6992921bda64663344ec
FluffelPuff <113269946+fluffelpuff@users.noreply.github.com>
2022-12-15 13:13:53 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,27 +1,29 @@
 import datetime
 
-# Die Aktuelle Uhrzeit wird als Datetime abgerufen
-now = datetime.datetime.now()
+# Erzeugt einen Byted Timestamp
+def get_current_timestamp():
+    # Die Aktuelle Uhrzeit wird als Datetime abgerufen
+    now = datetime.datetime.now()
 
-# Der Datetime Wert wird Formatiert und gesplittet
-low = (now.strftime('%Y-%m-%d-%H-%M-%S')).split('-')
+    # Der Datetime Wert wird Formatiert und gesplittet
+    low = (now.strftime('%Y-%m-%d-%H-%M-%S')).split('-')
 
-# Die Einzelnen Einträge werden abgerufen
-y = low.pop(0)
-m = low.pop(0)
-d = low.pop(0)
-h = low.pop(0)
-m = low.pop(0)
-s = low.pop(0)
+    # Die Einzelnen Werte werden in Bits umgewandelt
+    bited_year = "{0:b}".format(int(low.pop(0))).zfill(6*3)
+    bited_month = "{0:b}".format(int(low.pop(0))).zfill(4)
+    bited_day = "{0:b}".format(int(low.pop(0))).zfill(5)
+    bited_hour = "{0:b}".format(int(low.pop(0))).zfill(5)
+    bited_minute = "{0:b}".format(int(low.pop(0))).zfill(6)
+    bited_second = "{0:b}".format(int(low.pop(0))).zfill(6)
 
-# Die Einzelnen Werte werden in Bits umgewandelt
-bited_y = "{0:b}".format(int(y))
-bited_m = "{0:b}".format(int(m))
-bited_d = "{0:b}".format(int(d))
-bited_h = "{0:b}".format(int(h))
-bited_m = "{0:b}".format(int(m))
-bited_s = "{0:b}".format(int(s))
+    # Die Bitwerte werden aufbereitet
+    total_bit_string = "".join([bited_year, bited_month, bited_day, bited_hour, bited_minute, bited_second]).zfill(48)
+    splited_total_string = [total_bit_string[i:i+8] for i in range(0, len(total_bit_string), 8)]
+    inted_value = [int(i, 2) for i in splited_total_string]
+    byted_value = bytes(inted_value)
+    return byted_value.hex()
 
-# Die Bitwerte werden aufbereitet
 
-print(bited_y, bited_m, bited_d, bited_h ,bited_m, bited_s)
+
+current_timestamp = get_current_timestamp()
+print(current_timestamp)
